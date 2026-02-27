3ea748e7b7f8089d4764b74889fcb32c141c900a
ouattaraissouf7507 <ouattaraissouf7507@gmail.com>
2026-02-27 11:59:41 +0000

    lpo

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [
+    {
+      name: "ecom",
+      script: "pnpm",
+      args: "start",
+      cwd: __dirname,
+      instances: 1,
+      exec_mode: "fork",
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      node_args: "--max-old-space-size=896",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3005, // ✅ ici
+      },
+      error_file: "./logs/error.log",
+      out_file: "./logs/out.log",
+      merge_logs: true,
+      time: true,
+    },
+  ],
+};
